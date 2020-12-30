e6aa9a7e535dd525f20150366b2c51f40c9a9121
Evan <Evancat77@gmail.com>
2020-12-30 17:23:28 -0500

    enumeration and enum methods aaa

--- src/enumerator/index.d.ts
+++ src/enumerator/index.d.ts
@@ -1,22 +1,37 @@
 //I'm really sorry if you're looking at this
-type FakeEnum = {};
-type EnumeratorValues = ReadonlyArray<string> | { [key: string]: unknown };
+interface FakeEnum<T> {
+	value: T;
+	rawValue(): T;
+}
+
+interface SharedEnumerationMethods {
+	isEnumValue(value: unknown): boolean;
+}
+
+type AcceptableEnumeratorValues = ReadonlyArray<string> | { [key: string]: unknown };
 
 type ArrayElementType<T> = T extends ReadonlyArray<infer U> ? U : never;
 
-type LiteralToEnumeration<T extends EnumeratorValues> = {
-	readonly [key in ArrayElementType<T>]: FakeEnum;
-};
+type LiteralEnumeration<T extends AcceptableEnumeratorValues> = {
+	readonly [K in ArrayElementType<T>]: FakeEnum<string>;
+} & {
+	fromRawValue<K extends ArrayElementType<T>>(key: K): string;
+} & SharedEnumerationMethods;
 
+type DictionaryEnumeration<T> = {
+	readonly [K in keyof T]: FakeEnum<T[K]>;
+} & {
+	fromRawValue<K extends keyof T>(key: K): T[K];
+} & SharedEnumerationMethods;
 /**
  * 	Creates a new enumeration
  * @param name The unique name of the enumeration
  * @param values The values of the enumeration
  * @returns a new enumeration
  */
-declare function enumerator<T extends EnumeratorValues>(
+declare function enumerator<T extends AcceptableEnumeratorValues>(
 	name: string,
 	values: T,
-): T extends ReadonlyArray<string> ? LiteralToEnumeration<T> : Readonly<T>;
+): T extends ReadonlyArray<string> ? LiteralEnumeration<T> : DictionaryEnumeration<T>;
 
 export = enumerator;
