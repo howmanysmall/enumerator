bb6e7f24d579e9c93fec1a750fc85a904c5801be
HowManySmall <lhamil29@bruinmail.slcc.edu>
2021-01-08 19:21:27 -0700

    Fixed a dumb error.

--- src/enumerator/init.lua
+++ src/enumerator/init.lua
@@ -1,7 +1,17 @@
 -- enumerations in pure Luau
 -- @docs https://roblox.github.io/enumerate/
 
-local t = require(script.t)
+local t
+
+local module = assert(script:FindFirstChild("t"), "t wasn't found!")
+if module:IsA("ModuleScript") then
+	t = require(module)
+else
+	local ts = module:FindFirstChild("ts")
+	if ts and ts:IsA("ModuleScript") then
+		t = require(ts)
+	end
+end
 
 local ALREADY_USED_NAME_ERROR = "Already used %q as a value name in enum %q."
 local ALREADY_USED_VALUE_ERROR = "Already used %q as a value in enum %q."
